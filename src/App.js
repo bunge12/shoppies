c4e93d1dafe43b02004c78a90642d374b939f2d2
Artur <54365622+bunge12@users.noreply.github.com>
2020-08-28 10:48:40 -0400

    Added axios and return search from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
+import Search from "./components/Search";
+import Results from "./components/Results";
+import Nominations from "./components/Nominations";
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [nominations, setNominations] = useState([]);
+  // const [search, setSearch] = useState("");
+  const [results, setResults] = useState({});
+  const searchCallback = (data) => {
+    console.log(data);
+    axios
+      .get(`http://www.omdbapi.com/?apikey=789b871e&type=movie&s=${data}`)
+      .then((data) => {
+        console.log(data.data);
+        setResults(data.data);
+      });
+  };
+  return (
+    <div className="App">
+      Shoppies
+      <Search callback={searchCallback}></Search>
+      <Results data={results["Search"]}></Results>
+      <Nominations data={nominations}></Nominations>
+    </div>
+  );
 }
 
 export default App;
